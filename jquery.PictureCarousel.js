4c02f5f9f248c7ca66d1d8985786b48880572b9e
szynszyliszys <amjmigas@gmail.com>
2013-03-15 14:03:58 +0100

    Changing previous elements to active.

--- jquery.PictureCarousel.js
+++ jquery.PictureCarousel.js
@@ -7,18 +7,32 @@
     if(activeElements.length == 0) {
       throw new Error("There's no active element in the carousel, please set active class to one <li> tag");
     } else if(activeElements.length > 1) {
-      throw new Error("There's is more than one active element in the carousel, please set active class to one <li> tag");
+      throw new Error("There's is more than one active element in the carousel, please remove active class from a <li> tag");
     }
 
     
 
-    // return this.each(function() {
-    //   this. = $($(this).attr('.active'));
-    //   $(this).click(klik);
-    // });
+    return $this.each(function() {
+      var previous = $this.find("a.prev");
+      var next = $this.find("a.next");
+      
+      previous.click(left);
+      // next.click(right);
 
-    // function klik() {
+      function left() {
+        if(activeElements.prev().length === 0) {
+          $(".elementy").append(activeElements); //can I do that?
+        }
 
-    // }
+
+        activeElements.prev().addClass("active");
+        activeElements.removeClass("active");
+
+        activeElements = $this.find("li.active");
+
+      };
+    });
   };
 })(jQuery);
+
+//this. = $($(this).attr('.active'));
